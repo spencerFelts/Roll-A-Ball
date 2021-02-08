69dd71ac7f12ae16ee66df0f7235bc1a3646d184
spencerFelts <71102799+spencerFelts@users.noreply.github.com>
2021-02-07 21:20:58 -0600

    Add Project

---
+++ Roll-A-Ball [Spencer Felts]/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 800.0f;
+    public Text scoreText;
+    private int count = 0;
+    public Text winText;
+
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            scoreText.text = "Score: " + count;
+        }
+
+        if (count >= 12)
+        {
+            winText.gameObject.SetActive(true);
+        }
+    }
+}
